084d8c5e827048c762fc4147dfa5aeac743d00d3
shorifulsobuj <sobujaiom126@gmail.com>
2024-03-07 23:47:12 +0600

    E-commerce v0.5

--- ECOMMERCE/classes/loginClass.php
+++ ECOMMERCE/classes/loginClass.php
@@ -1,1 +1,20 @@
-rfc
+<?php
+
+namespace class\login;
+
+require_once 'db.php';
+
+class loginClass
+{
+    public function login($email, $password)
+    {
+        $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
+        $result = $conn->query($sql);
+        $row = $result->fetch_assoc();
+        if ($row['email'] == $email && $row['password'] == $password) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ ECOMMERCE/classes/registrationClass.php
@@ -0,0 +1,9 @@
+<?php
+
+class registationClass
+{
+    public function register($name, $email, $password,)
+    {
+        
+    }
+}
